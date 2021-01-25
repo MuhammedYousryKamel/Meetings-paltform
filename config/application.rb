7f2fc3af296206bb1c32ebfaa7f8988c0db3188c
Muhammed Yousry <myousry@Muhammeds-MacBook-Pro.local>
2021-01-25 12:50:56 +0200

    add the partials views for meetings dashboard and solve the issues of tailwind css

--- config/application.rb
+++ config/application.rb
@@ -9,9 +9,10 @@ Bundler.require(*Rails.groups)
 module MeetMe
   class Application < Rails::Application
     config.active_job.queue_adapter = :sidekiq
+    config.application_name = 'MeetMe'
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
-
+    
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
